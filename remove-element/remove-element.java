7742870b23420a25af3d112173be0f2480299ba7
Dragonspine <43735560+Dragonspine@users.noreply.github.com>
2022-06-06 00:41:57 +0900

    Time: 0 ms, Memory: 42.5 MB - LeetHub

--- remove-element/remove-element.java
+++ remove-element/remove-element.java
@@ -1,8 +1,5 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int p1 = 0;
-        int p2 = nums.length - 1;
-
         int p = 0;
 
         for (int i = 0; i < nums.length; i++) {
