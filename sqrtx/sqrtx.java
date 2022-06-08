14aedf7a8f3a9117353d14d66b991360e438ef10
Dragonspine <43735560+Dragonspine@users.noreply.github.com>
2022-06-08 22:23:38 +0900

    Time: 43 ms, Memory: 41 MB - LeetHub

--- sqrtx/sqrtx.java
+++ sqrtx/sqrtx.java
@@ -3,11 +3,10 @@ class Solution {
         if(x == 0 ){
             return 0;
         }
-        if (x == 2147483647) {
-            return 46340;
-        }
         int num = 1;
         while (num * num < x && (num + 1) * (num + 1) <= x) {
+            if ((num + 1) * (num + 1) < 0)
+                return num;
             num++;
         }
         return num;
