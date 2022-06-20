c6528840d1a84ec3b81a2341553c7d3f0adda7cd
Dragonspine <43735560+Dragonspine@users.noreply.github.com>
2022-06-20 22:21:58 +0900

    Time: 2 ms, Memory: 48.1 MB - LeetHub

---
+++ sort-array-by-parity/sort-array-by-parity.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        if (nums == null || nums.length == 1) {
+            return nums;
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            for (int i = left; i < right; i++) {
+                if (nums[left] % 2 == 1) {
+                    break;
+                } else {
+                    left++;
+                }
+            }
+
+            for (int i = right; i > left; i--) {
+                if (nums[right] % 2 == 0) {
+                    break;
+                } else {
+                    right--;
+                }
+            }
+
+            if (nums[left] % 2 == 1 && nums[right] % 2 == 0) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+            }
+        }
+
+        return nums;
+    }
+}
