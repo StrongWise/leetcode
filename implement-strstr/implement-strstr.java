25412fc375c1cadf73bb45e60b87c2fb1e946dbf
Dragonspine <43735560+Dragonspine@users.noreply.github.com>
2022-07-06 23:50:03 +0900

    Time: 2 ms, Memory: 42.1 MB - LeetHub

--- implement-strstr/implement-strstr.java
+++ implement-strstr/implement-strstr.java
@@ -1,5 +1,26 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+        if (haystack.length() < needle.length()) {
+            return -1;
+        }
+
+        for (int i = 0; i < haystack.length(); i++) {
+            if (haystack.charAt(i) == needle.charAt(0)) {
+                if (haystack.length() - i < needle.length()) {
+                    return -1;
+                }
+                int j = 0;
+                for (; j < needle.length(); j++) {
+                    if (haystack.charAt(i + j) != needle.charAt(j)) {
+                        break;
+                    }
+                }
+                if (j == needle.length()) {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
     }
 }
