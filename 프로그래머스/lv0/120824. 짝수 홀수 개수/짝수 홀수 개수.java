1096fa89ed8a33b44f56f14a9e8ac1c172735e3f
StrongWise <43735560+StrongWise@users.noreply.github.com>
2023-04-24 09:24:33 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int odd = 0;
+        int even = 0;
+        for (int i : num_list) {
+            if (i % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        return new int[]{even, odd};
+    }
+}
