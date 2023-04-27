960597ea662a8da97a24f69abc63f0540bd54d50
StrongWise <43735560+StrongWise@users.noreply.github.com>
2023-04-27 13:58:29 +0900

    [level 0] Title: 합성수 찾기, Time: 0.02 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
@@ -1,20 +1,18 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-
         for (int i = 1; i < n + 1; i++) {
             int count = 0;
             for (int j = 1; j < i + 1; j++) {
                 if (i % j == 0) {
                     count++;
                 }
-                if (count >= 3) {
+                if (count > 2) {
                     answer++;
                     break;
                 }
             }
         }
-
         return answer;
     }
 }
