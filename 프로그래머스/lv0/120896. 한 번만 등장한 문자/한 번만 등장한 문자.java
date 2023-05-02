35d11f83ce8c2b8f2378a2c23ff1efc7955f3758
StrongWise <43735560+StrongWise@users.noreply.github.com>
2023-05-02 11:04:35 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 1.71 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -3,12 +3,19 @@ import java.util.Map;
 import java.util.stream.Collectors;
 class Solution {
     public String solution(String s) {
-        return Arrays.stream(s.split(""))
-                .collect(Collectors.groupingBy(String::valueOf, Collectors.counting()))
-                .entrySet().stream()
-                .filter(e -> e.getValue() == 1)
-                .sorted(Map.Entry.comparingByKey())
-                .map(Map.Entry::getKey)
-                .collect(Collectors.joining());
+        String answer = "";
+        int[] alphabet = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            alphabet[s.charAt(i)-97]++;
+        }
+
+        for (int i = 0; i < alphabet.length; i++) {
+            if (alphabet[i] == 1) {
+                answer += Character.toString(i + 97);
+            }
+        }
+
+        return answer;
     }
 }
