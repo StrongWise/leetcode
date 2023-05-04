c6e9506985646dca09d58e992a8ad4647b2b6ac5
StrongWise <43735560+StrongWise@users.noreply.github.com>
2023-05-04 10:48:19 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,12 +1,6 @@
 class Solution {
     public int solution(int num, int k) {
-        String s = String.valueOf(num);
-        for (int i = 0; i < s.length(); i++) {
-            if (Integer.parseInt(s.charAt(i) + "") == k) {
-                return i + 1;
-            }
-        }
-
-        return -1;
+        int i = String.valueOf(num).indexOf(String.valueOf(k));
+        return i == -1 ? i : i +1;
     }
 }
