e26fe6c7a8f8eb766efc0b16091c4d1c9d5ed10c
StrongWise <43735560+StrongWise@users.noreply.github.com>
2023-05-08 10:44:54 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 2.61 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,6 @@
+import java.util.stream.Collectors;
+class Solution {
+    public String solution(String my_string) {
+        return my_string.toLowerCase().chars().sorted().mapToObj(Character::toString).collect(Collectors.joining());
+    }
+}
