4cf0002f0115248b93031fee5be3d47631e5a9bc
StrongWise <43735560+StrongWise@users.noreply.github.com>
2023-05-09 13:44:41 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.30 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -1,57 +1,31 @@
 class Solution {
     public int[] solution(String[] keyinput, int[] board) {
         int[] answer = {0,0};
-        int boundaryX = (board[0] - 1) / 2;
-        int boundaryY = (board[1] - 1) / 2;
 
         for (String key : keyinput) {
             switch (key) {
                 case "up":
                     System.out.println("up");
-                    if (answer[1] >= 0) {
-                        if (answer[1] < boundaryY) {
-                            answer[1]++;
-                        }
-                    } else {
-                        if (answer[1] >= -1 * boundaryY) {
-                            answer[1]++;
-                        }
+                    if (answer[1] < (board[1] - 1) / 2) {
+                        answer[1]++;
                     }
                     break;
                 case "down":
                     System.out.println("down");
-                    if (answer[1] >= 0) {
-                        if (answer[1] <= boundaryY) {
-                            answer[1]--;
-                        }
-                    } else {
-                        if (answer[1] > -1 * boundaryY) {
-                            answer[1]--;
-                        }
+                    if (answer[1] > -1 * ((board[1] - 1) / 2)) {
+                        answer[1]--;
                     }
                     break;
                 case "left":
                     System.out.println("left");
-                    if (answer[0] >= 0) {
-                        if (answer[0] <= boundaryX) {
-                            answer[0]--;
-                        }
-                    } else {
-                        if (answer[0] > -1 * boundaryX) {
-                            answer[0]--;
-                        }
+                    if (answer[0] > -1 * ((board[0] - 1) / 2)) {
+                        answer[0]--;
                     }
                     break;
                 case "right":
                     System.out.println("right");
-                    if (answer[0] >= 0) {
-                        if (answer[0] < boundaryX) {
-                            answer[0]++;
-                        }
-                    } else {
-                        if (answer[0] >= -1 * boundaryX) {
-                            answer[0]++;
-                        }
+                    if (answer[0] < (board[0] - 1) / 2) {
+                        answer[0]++;
                     }
                     break;
                 default:
