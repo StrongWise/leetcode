f215d2e648615331a0d4420d78a42888c3264015
StrongWise <43735560+StrongWise@users.noreply.github.com>
2023-05-10 14:11:50 +0900

    [level 0] Title: 평행, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.java
+++ 프로그래머스/lv0/120875. 평행/평행.java
@@ -1,29 +1,18 @@
 class Solution {
     public int solution(int[][] dots) {
-        double x1, x2, y1, y2 = 0;
-        x1 = dots[0][0] - dots[1][0];
-        x2 = dots[0][1] - dots[1][1];
-        y1 = dots[2][0] - dots[3][0];
-        y2 = dots[2][1] - dots[3][1];
-        if (x1 / x2 == y1 / y2) {
+        if (isSameTilt(dots[0], dots[1], dots[2], dots[3])) {
+            return 1;
+        }
+        if (isSameTilt(dots[0], dots[2], dots[1], dots[3])) {
+            return 1;
+        }
+        if (isSameTilt(dots[0], dots[3], dots[1], dots[2])) {
             return 1;
         }
 
-        x1 = dots[0][0] - dots[2][0];
-        x2 = dots[0][1] - dots[2][1];
-        y1 = dots[1][0] - dots[3][0];
-        y2 = dots[1][1] - dots[3][1];
-        if (x1 / x2 == y1 / y2) {
-            return 1;
-        }
-
-        x1 = dots[0][0] - dots[3][0];
-        x2 = dots[0][1] - dots[3][1];
-        y1 = dots[1][0] - dots[2][0];
-        y2 = dots[1][1] - dots[2][1];
-        if (x1 / x2 == y1 / y2) {
-            return 1;
-        }
         return 0;
+    }
+    private static boolean isSameTilt(int[] dot1, int[] dot2, int[] dot3, int[] dot4) {
+        return (double) (dot1[0] - dot2[0]) / (dot1[1] - dot2[1]) == (double) (dot3[0] - dot4[0]) / (dot3[1] - dot4[1]);
     }
 }
