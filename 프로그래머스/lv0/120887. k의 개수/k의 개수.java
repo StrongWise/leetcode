865e2250cb3fd8137f16d103b69eb5a92a3d419a
StrongWise <43735560+StrongWise@users.noreply.github.com>
2023-05-11 11:51:47 +0900

    [level 0] Title: k의 개수, Time: 5.79 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -1,10 +1,9 @@
-import java.util.Arrays;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.IntStream;
 class Solution {
     public int solution(int i, int j, int k) {
-        AtomicInteger answer = new AtomicInteger();
-        IntStream.rangeClosed(i, j).forEach(n -> answer.addAndGet((int) Arrays.stream(String.valueOf(n).split("")).filter(e -> Integer.parseInt(e) == k).count()));
-        return answer.get();
+        StringBuilder sb = new StringBuilder();
+        for (int n = i; n <= j; n++) {
+            sb.append(n);
+        }
+        return sb.toString().length() - sb.toString().replace(String.valueOf(k), "").length();
     }
 }
