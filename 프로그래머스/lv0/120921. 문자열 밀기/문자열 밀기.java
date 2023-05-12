c53c19355e5524e088d44c44c87e6b46065e333d
StrongWise <43735560+StrongWise@users.noreply.github.com>
2023-05-12 10:18:49 +0900

    [level 0] Title: 문자열 밀기, Time: 0.02 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,17 +1,5 @@
 class Solution {
     public int solution(String A, String B) {
-        int answer = 0;
-
-        if (A.equals(B))
-            return 0;
-            
-        for (int i = 0; i < A.length(); i++) {
-            A = A.charAt(A.length() - 1) + A.substring(0, A.length() - 1);
-            answer++;
-            if (A.equals(B)) {
-                return answer;
-            }
-        }
-        return -1;
+        return B.repeat(2).indexOf(A);
     }
 }
