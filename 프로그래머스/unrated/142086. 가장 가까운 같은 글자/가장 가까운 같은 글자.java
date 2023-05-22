d9cb7808bf7a4e4e61611d0eb54cef24bc2d1b63
StrongWise <43735560+StrongWise@users.noreply.github.com>
2023-05-22 12:52:23 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 4.60 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c = 'a'; c < 'a'+ 26; c++) {
+            map.put(c, -1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.get(c) == -1) {
+                answer[i] = -1;
+            } else {
+                answer[i] = i - map.get(c);
+            }
+            map.put(c, i);
+        }
+
+        return answer;
+    }
+}
