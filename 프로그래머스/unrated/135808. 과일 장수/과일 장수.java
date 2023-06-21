dc289e82e7da65cfa43fd95ae5937b4d8d826426
StrongWise <43735560+StrongWise@users.noreply.github.com>
2023-06-21 09:16:34 +0900

    [unrated] Title: 과일 장수, Time: 0.57 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -3,10 +3,8 @@ class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
         Arrays.sort(score);
-        for (int i = score.length-1; i >= 0; i--) {
-            if ((score.length - i) % m == 0) {
-                answer += score[i] * m;
-            }
+        for (int i = score.length - m; i >= 0; i-=m) {
+            answer += score[i] * m;
         }
         return answer;
     }
