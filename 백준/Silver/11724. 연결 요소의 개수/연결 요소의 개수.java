cece41a358af65a78b67f5ca6db57479efa5ec83
StrongWise <43735560+StrongWise@users.noreply.github.com>
2024-01-16 17:11:33 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 668 ms, Memory: 141268 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,53 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static boolean visited[];
+
     static ArrayList<Integer>[] A;
+    static boolean[] visited;
+    static int N, M;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        visited = new boolean[n + 1];
-        A = new ArrayList[n + 1];
-        for (int i = 1; i < n + 1; i++) {
+        A = new ArrayList[N + 1];
+
+        for (int i = 1; i < N + 1; i++) {
             A[i] = new ArrayList<>();
         }
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
+        visited = new boolean[N + 1];
 
-            A[s].add(e);
-            A[e].add(s);
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            A[u].add(v);
+            A[v].add(u);
         }
 
         int count = 0;
-
-        for (int i = 1; i < n + 1; i++) {
+        for (int i = 1; i < N + 1; i++) {
             if (!visited[i]) {
+                visited[i] = true;
                 count++;
-                DFS(i);
+                
+                Stack<Integer> stack = new Stack<>();
+                stack.add(i);
+                while (!stack.isEmpty()) {
+                    Integer pop = stack.pop();
+                    for (int j : A[pop]) {
+                        if (!visited[j]) {
+                            visited[j] = true;
+                            stack.add(j);
+                        }
+                    }
+                }
+
             }
         }
         System.out.println(count);
-    }
-
-    private static void DFS(int v) {
-        if (visited[v]) {
-            return;
-        }
-        visited[v] = true;
-
-        for (int i : A[v]) {
-            if (!visited[i]) {
-                DFS(i);
-            }
-        }
     }
 }
