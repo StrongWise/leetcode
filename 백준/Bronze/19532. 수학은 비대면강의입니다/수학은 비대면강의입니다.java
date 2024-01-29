541c6cd80c2e12e8c496789586c38aaa48f94fbe
StrongWise <43735560+StrongWise@users.noreply.github.com>
2024-01-29 15:16:55 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 200 ms, Memory: 16384 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int a = Integer.parseInt(s[0]);
+        int b = Integer.parseInt(s[1]);
+        int c = Integer.parseInt(s[2]);
+        int d = Integer.parseInt(s[3]);
+        int e = Integer.parseInt(s[4]);
+        int f = Integer.parseInt(s[5]);
+
+        for (int x = -999; x <= 999; x++) {
+            for (int y = -999; y <= 999; y++) {
+                if (a * x + b * y == c && d * x + e * y == f) {
+                    System.out.println(x + " " + y);
+                }
+            }
+        }
+    }
+}
