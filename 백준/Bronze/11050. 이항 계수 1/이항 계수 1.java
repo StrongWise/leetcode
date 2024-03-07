a089777833f42e08075a019c4967295f3b822096
StrongWise <43735560+StrongWise@users.noreply.github.com>
2024-03-07 11:21:18 +0900

    [Bronze I] Title: 이항 계수 1, Time: 204 ms, Memory: 17668 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,25 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int K = sc.nextInt();
-        int[][] D = new int[N + 1][N + 1]; // N 개중 K 개를 뽑는 DP 배열
 
-        for (int i = 0; i < N + 1; i++) {
-            D[i][1] = i;
-            D[i][0] = 1;
-            D[i][i] = 1;
+        int answer = 1;
+        for (int i = N; i > K; i--) {
+            answer *= i;
         }
-
-        for (int i = 2; i < N + 1; i++) {
-            for (int j = 1; j < i; j++) {
-                D[i][j] = D[i - 1][j] + D[i - 1][j - 1];
-            }
+        for (int i = N - K; i > 0; i--) {
+            answer /= i;
         }
-
-        System.out.println(D[N][K]);
+        System.out.println(answer);
     }
 }
