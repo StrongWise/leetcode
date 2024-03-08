dd1c007c2d597c3dd8b8f55f5ed9d7b831153758
StrongWise <43735560+StrongWise@users.noreply.github.com>
2024-03-08 16:52:21 +0900

    [Silver III] Title: N과 M (3), Time: 468 ms, Memory: 69144 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+
+        N = Integer.parseInt(str[0]);
+        M = Integer.parseInt(str[1]);
+
+        arr = new int[M];
+
+        dfs(0);
+
+        System.out.println(sb);
+    }
+
+    private static void dfs(int depth) {
+        if (depth == M) {
+            for (int i : arr) {
+                sb.append(i).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            arr[depth] = i + 1;
+            dfs(depth + 1);
+        }
+    }
+}
